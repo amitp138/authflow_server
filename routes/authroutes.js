8f4603545b8f8ca4faee6a8c3a2ccc8d7860f397
amitp138 <amitpande1008@gmail.com>
2024-01-10 11:51:29 +0530

    remove tls

--- routes/authroutes.js
+++ routes/authroutes.js
@@ -68,10 +68,7 @@ router.post("/email/welcome", async (req, res) => {
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASSWORD,
-    },
-    tls: {
-      ciphers: "SSLv3",
-    },
+    }
   });
 
   const mailOptions = {
