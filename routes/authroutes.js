541f894f6c7c1680a89385a66344f228ac088f2e
amitp138 <amitpande1008@gmail.com>
2024-01-10 13:09:05 +0530

    make secure false

--- routes/authroutes.js
+++ routes/authroutes.js
@@ -65,7 +65,7 @@ router.post("/email/welcome", async (req, res) => {
   const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
-    secure: true,
+    secure: false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASSWORD,
